80762d4296f7d7ffe728a852e1025e4ee08cc7ee
FRVR-JAE <tjoloyede9@gmail.com>
2024-03-05 21:21:38 -0800

    1

---
+++ client/src/layouts/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function layout(props) {
+  return (
+    <div>layout</div>
+  )
+}
+
+layout.propTypes = {}
+
+export default layout
