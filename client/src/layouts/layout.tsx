b4bbd617ff0b59cdd613054a26cc5ae185d31182
FRVR-JAE <tjoloyede9@gmail.com>
2024-03-10 12:47:14 -0700

    up

--- client/src/layouts/layout.tsx
+++ client/src/layouts/layout.tsx
@@ -1,17 +1,18 @@
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 type Props = {
-children: React.ReactNode;
-}
+  children: React.ReactNode;
+};
 
-const Layout = ({children}: Props) => {
+const Layout = ({ children }: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
-       <Header />
-
-       <div className="container mx-auto flex-1 py-10">{children}</div>
+      <Header />
+      <Hero />
+      <div className="container mx-auto flex-1 py-10">{children}</div>
     </div>
-  )
-}
+  );
+};
 
 export default Layout;
